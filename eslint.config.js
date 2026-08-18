de0e9590f4bde6f35decd5247c4865f6de3c1324
agneja00 <agnieshaa00@gmail.com>
2026-08-18 21:00:03 +0300

    Chore: init vite + react + scss project

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from 'globals';
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from 'eslint-config-prettier'
+
+export default [
+   { ignores: ['dist', 'node_modules', '*.config.js'] },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+     languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react-refresh/only-export-components": "warn",
+    },
+    settings: { react: { version: "detect" } },
+  },
+   prettier,
+];
