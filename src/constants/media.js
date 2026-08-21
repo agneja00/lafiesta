6749e2db7734841d37d2de38e483d382009afe51
agneja00 <agnieshaa00@gmail.com>
2026-08-22 01:46:41 +0300

    Add icons to media

--- src/constants/media.js
+++ src/constants/media.js
@@ -1,5 +1,23 @@
 const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 
-export const LOGO_URL = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/f_auto,q_auto,w_160/logoo_aoovzs`;
-export const HERO_URL = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/f_auto,q_auto/heroo_wqvkfe`;
-export const HERO_DESKTOP = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/f_auto,q_auto/hero_desktop_xhnwvj`;
+const CLOUDINARY_BASE = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload`;
+
+export const LOGO_URL = `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/logoo_aoovzs`;
+export const HERO_URL = `${CLOUDINARY_BASE}/f_auto,q_auto/heroo_wqvkfe`;
+export const HERO_DESKTOP = `${CLOUDINARY_BASE}/f_auto,q_auto/hero_desktop_xhnwvj`;
+
+export const ICONS = {
+  section1: {
+    card1: `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/s1flower_dmzicu`,
+    card2: `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/s1gift_scj9g5`,
+    card3: `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/s1balloon_yfnjj9`,
+    card4: `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/s1candy_rjhnux`,
+  },
+
+  section2: {
+    card1: `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/s2heart_o3jvi7`,
+    card2: `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/s2individual_ekc3jo`,
+    card3: `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/s2fresh_pimwcj`,
+    card4: `${CLOUDINARY_BASE}/f_auto,q_auto,w_160/s2gift_m7bfof`,
+  },
+};
