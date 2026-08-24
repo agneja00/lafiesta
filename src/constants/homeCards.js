87ec5eaff86e536aa9593cdbcc78b2d7fa495f7d
agneja00 <agnieshaa00@gmail.com>
2026-08-24 22:21:54 +0300

    Add homeCards and translates for them

---
+++ src/constants/homeCards.js
@@ -0,0 +1,61 @@
+import { ICONS } from "./media";
+
+export const HOME_CREATE_CARDS = [
+  {
+    id: "flowers",
+    icon: ICONS.section1.card1,
+    titleKey: "createCards.flowers.title",
+    descriptionKey: "createCards.flowers.description",
+  },
+
+  {
+    id: "gifts",
+    icon: ICONS.section1.card2,
+    titleKey: "createCards.gifts.title",
+    descriptionKey: "createCards.gifts.description",
+  },
+
+  {
+    id: "balloons",
+    icon: ICONS.section1.card3,
+    titleKey: "createCards.balloons.title",
+    descriptionKey: "createCards.balloons.description",
+  },
+
+  {
+    id: "sweetGifts",
+    icon: ICONS.section1.card4,
+    titleKey: "createCards.sweetGifts.title",
+    descriptionKey: "createCards.sweetGifts.description",
+  },
+];
+
+export const HOME_REASONS = [
+  {
+    id: "personal",
+    icon: ICONS.section2.card1,
+    titleKey: "reasons.personal.title",
+    descriptionKey: "reasons.personal.description",
+  },
+
+  {
+    id: "individual",
+    icon: ICONS.section2.card2,
+    titleKey: "reasons.individual.title",
+    descriptionKey: "reasons.individual.description",
+  },
+
+  {
+    id: "fresh",
+    icon: ICONS.section2.card3,
+    titleKey: "reasons.fresh.title",
+    descriptionKey: "reasons.fresh.description",
+  },
+
+  {
+    id: "choice",
+    icon: ICONS.section2.card4,
+    titleKey: "reasons.choice.title",
+    descriptionKey: "reasons.choice.description",
+  },
+];
